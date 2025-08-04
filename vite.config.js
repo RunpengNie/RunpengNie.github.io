cfb3c615a69721dcb7cc6b5493a1f4e3627cbac3
RunpengNie <707744613nrp@gmail.com>
2025-08-03 20:20:18 -0600

    fix gallerysystem

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
-const repoName = '';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: `/`
-});
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        const indexPath = resolve(__dirname, 'dist/index.html')
+        const notFoundPath = resolve(__dirname, 'dist/404.html')
+        if (fs.existsSync(indexPath)) {
+          fs.copyFileSync(indexPath, notFoundPath)
+          // console.log('404.html created')
+        }
+      }
+    }
+  ],
+  base: './', // GitHub Pages 推荐 base 用相对路径
+})
